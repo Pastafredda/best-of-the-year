75848e7365e5bc327ddf8480b31f775a936bf0fb
Pastafredda <justspotify09@gmail.com>
2023-11-07 20:10:28 +0100

    step 2

--- src/main/java/com/experis/course/bestoftheyear/controller/IndexController.java
+++ src/main/java/com/experis/course/bestoftheyear/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
@@ -12,6 +14,36 @@ public class IndexController {
     @GetMapping("home")
     public String nome(Model model) {
         model.addAttribute("name", "Matteo");
+        return "index";
+    }
+
+    private ArrayList<String> getBestMovies() {
+        ArrayList<String> moviesList = new ArrayList<>();
+        moviesList.add("Fight Club");
+        moviesList.add("Django");
+        moviesList.add("Shutter Island");
+        moviesList.add("Carlito's Way");
+        return moviesList;
+    }
+
+    private ArrayList<String> getBestSongs() {
+        ArrayList<String> songsList = new ArrayList<>();
+        songsList.add("The man who sold the world");
+        songsList.add("Stairway to Heaven");
+        songsList.add("Comforably Numb");
+        songsList.add("I Say I' Sto Ccà");
+        return songsList;
+    }
+
+    @GetMapping("/movies")
+    public String movies(Model model) {
+        model.addAttribute("movies", getBestMovies());
+        return "index";
+    }
+
+    @GetMapping("/songs")
+    public String songs(Model model) {
+        model.addAttribute("songs", getBestSongs());
         return "index";
     }
 }
